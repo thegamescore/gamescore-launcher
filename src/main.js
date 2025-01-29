ce8b9e6d3b7feb33e12ce3308cf6953393929df8
mstosio <michal@mintra.com>
2025-01-29 21:03:05 +0100

    Initial louncher

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// const GAMESCORE_LOUNCHER_CLASSNAME = 'gamescore-loucher'
+//
+//
+// function appendGameScoreLouncherDiv(){
+//     const div = document.createElement('button');
+//     div.classList.add(GAMESCORE_LOUNCHER_CLASSNAME);
+//     document.body.appendChild(div);
+// }
+//
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//     appendGameScoreLouncherDiv()
+// });
